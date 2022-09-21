9794aac8b2a4fbb933213a240bb2f8b2d577f829
Júlia Ohara <juliaohara.br@gmail.com>
2022-09-20 21:35:03 -0300

    feat: add error message to url not found

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const form = document.querySelector('.register');
 const input = document.querySelector('.valueInput');
 
+function clearInput() {
+  input.value = '';
+}
+
 function openConsole() {
   const valueInput = input.value;
 
-  const options = {method: 'GET'};
+  if (valueInput.length != null && valueInput.length > 0) {
+    const options = {method: 'GET'};
 
-  fetch('https://pokeapi.co/api/v2/pokemon/' + valueInput, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    fetch('https://pokeapi.co/api/v2/pokemon/' + valueInput, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => generateMsgError());
+  }
+}
+
+function generateMsgError() {
+  const msgError = document.createElement("p");
+  const textError = "URL não encontrada, tente novamente!";
+  form.appendChild(msgError);
+  msgError.innerHTML = textError;
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  clearInput()
 })
